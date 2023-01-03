a09b6de85cf87e138797f44064c61d23324e668a
Dhagegren <dh222ph@student.lnu.se>
2023-01-03 12:48:11 +0100

    changes

--- js/Window.js
+++ js/Window.js
@@ -32,19 +32,19 @@ Window.prototype.createWindow = function () {
 
     this.diceToolbarWrapper.setAttribute("class", "dice-toolbar-wrapper");
     this.diceWindowWrapper.appendChild(this.diceToolbarWrapper);
-    
+
     this.diceToolbarWrapper.appendChild(this.ulList);
 
     this.add.setAttribute("class", "add");
     this.add.addEventListener("click", this.addDice.bind(this));
 
-    this.add.addEventListener("click", this.countPoints.bind(this));    
+    this.add.addEventListener("click", this.countPoints.bind(this));
     this.add.addEventListener("click", this.addSound.bind(this));
     this.ulList.appendChild(this.add);
 
     this.remove.setAttribute("class", "remove");
     this.remove.addEventListener("click", this.removeDice.bind(this));
-    this.remove.addEventListener("click",this.countPoints.bind(this));
+    this.remove.addEventListener("click", this.countPoints.bind(this));
     this.ulList.appendChild(this.remove);
 
     this.roll.setAttribute("class", "roll");
@@ -59,35 +59,42 @@ Window.prototype.createWindow = function () {
     this.emptyLi.appendChild(this.toolbarCounter);
 
     for (let i = 0; i < 5; i++) {
-         this.liZero = document.createElement("li");
+        this.liZero = document.createElement("li");
         this.liZero.setAttribute("class", "zero");
         this.toolbarCounter.appendChild(this.liZero);
     }
-    
-   
+
+
     this.diceContentWrapper.setAttribute("class", "dice-content-wrapper");
     this.diceWindowWrapper.appendChild(this.diceContentWrapper);
 
 
     this.diceContentWrapper.appendChild(this.diceUl);
     this.diceArr = [];
-    
+
 }
 
 //väldigt ofärdig kod för att fixa css på räknaren, inte en aning hur jag ska tackla problement.
-Window.prototype.countPoints = function (){
-   
-var letters = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-var points = 0;
+Window.prototype.countPoints = function () {
+    for (i = 0; i < this.toolbarCounter.children.length; i++) {
+        this.toolbarCounter.children[i].setAttribute("class", "zero");
+    }
 
-     for(i=0; i<this.diceArr.length; i++){
-        points += parseInt(this.diceArr[i].value); 
-      }
-      var pointsString =Array.from(String(points));
-      for(i=0; i<pointsString.length; i++){
+    var letters = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var points = 0;
+    var index = 4;
+
+    for (i = 0; i < this.diceArr.length; i++) {
+        points += parseInt(this.diceArr[i].value);
+    }
+    var pointsString = Array.from(String(points));
+    pointsString.reverse();
+    for (i = 0; i < pointsString.length; i++) {
         result = pointsString[i].replace(/\b\d\b/g, m => letters[m]);
-        this.toolbarCounter.children[i].setAttribute("class", result); 
-      }  
+        this.toolbarCounter.children[index].setAttribute("class", result);
+        index--;
+    }
+
 }
 
 Window.prototype.closeWindow = function () {
@@ -96,30 +103,30 @@ Window.prototype.closeWindow = function () {
 
 
 Window.prototype.addDice = function () {
-    if(this.diceArr.length <40){
-    var dice = new Dice();
-    dice.rollDice(dice);
-    this.diceArr.push(dice);
-    this.diceUl.appendChild(dice.diceElement);
+    if (this.diceArr.length < 40) {
+        var dice = new Dice();
+        dice.rollDice(dice);
+        this.diceArr.push(dice);
+        this.diceUl.appendChild(dice.diceElement);
     }
 }
 
-Window.prototype.removeDice = function (){
-    this.last = this.diceArr[this.diceArr.length -1];
+Window.prototype.removeDice = function () {
+    this.last = this.diceArr[this.diceArr.length - 1];
     this.last.diceElement.parentNode.removeChild(this.last.diceElement);
-    this.diceArr.pop(); 
- }
-
- Window.prototype.addSound = function(){
-    new Audio(src ="wav/add.wav").play();
+    this.diceArr.pop();
 }
 
- //tror det funkar nu, ändrar value direkt istället för att sätta ett attribut som en jävla apa
- Window.prototype.diceReRoll = function (){
-    for(i=0; i<this.diceArr.length; i++){
-        this.diceArr[i].reroll();         
+Window.prototype.addSound = function () {
+    new Audio(src = "wav/add.wav").play();
+}
+
+//tror det funkar nu, ändrar value direkt istället för att sätta ett attribut som en jävla apa
+Window.prototype.diceReRoll = function () {
+    for (i = 0; i < this.diceArr.length; i++) {
+        this.diceArr[i].reroll();
     }
- }
+}
 
 
 
